5ad71fe505e1aa1d71087809b20d9d0db69439ab
bfgiacon <beatriz.giacon@aluno.ufabc.edu.br>
2025-01-21 15:05:09 +0100

    Update character.h

--- character.h
+++ character.h
@@ -8,7 +8,7 @@ public:
 Character(std::string name, int strenght, int health) : name_(name), strenght_(strenght), health_(health) {}
 virtual ~Character(){}
 
-virtual void attack() const = 0; 
+virtual void attack(std::shared_ptr<Character> outro) const = 0; 
 
 virtual int setHealth() const = 0;
 
