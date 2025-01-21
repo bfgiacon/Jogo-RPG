06a3bdcb21d26a34b401c47bae5e911b39724417
bfgiacon <beatriz.giacon@aluno.ufabc.edu.br>
2025-01-21 15:08:29 +0100

    Update arquer.cpp

--- arquer.cpp
+++ arquer.cpp
@@ -1,8 +1,15 @@
 #include "arquer.h"
 #include <iostream>
 
-void Arquer::attack() const{
+void Arquer::attack(std::shared_ptr<Character> outro) const{
   std::cout << "Ataque com arco e flecha" << std::endl;
+  if(outro->strenght_ > strenght_){
+    health_ = health_ - 2;
+  } else if(outro->strenght_ == strenght_){
+    health_ = health_}
+  else {
+    outro->strenght_ -= 2;
+  }
 }
 
 int Arquer::setHealth() const{
